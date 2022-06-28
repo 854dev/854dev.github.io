75392b5d72313fcbcee01bfad5b66ed14b0b3f16
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-06-28 17:56:21 +0900

    baseUrl 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const baseUrl = '';
+const baseUrl = '/854dev.github.io';
 
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
