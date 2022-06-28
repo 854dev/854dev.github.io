d257d6f19f3fd84ae7b6f29acc19b69bd879b539
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-06-28 16:33:00 +0900

    build 220628

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,7 +10,7 @@ const About = () => (
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
         Quaerat molestiae blanditiis doloremque possimus labore voluptatibus distinctio recusandae
-        autem esse explicabo molestias officia placeat, accusamus aut saepe. 
+        autem esse explicabo molestias officia placeat, accusamus aut saepe.
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
