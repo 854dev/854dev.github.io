08a297c8ed7d7ba69c5532103f9fba76145c9a40
854 <jahae854@gmail.com>
2022-11-08 23:37:35 +0900

    프로젝트 비우기

--- src/utils/AppConfig.ts
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Next.js Boilerplate',
-  description: 'Starter code for your Next.js blog Boilerplate with Tailwind CSS',
-  url: 'https://example.com',
-  locale: 'en',
-  author: 'Anonymous',
-  pagination_size: 5,
-};
